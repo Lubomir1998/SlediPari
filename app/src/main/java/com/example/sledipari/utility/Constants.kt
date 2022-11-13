eaa64b80faacc0f5b89892f360a154f9f6350c92
Lubomir1998 <lubcho_sk8@abv.bg>
2022-11-13 18:00:54 +0200

    Use build config.

--- app/src/main/java/com/example/sledipari/utility/Constants.kt
+++ app/src/main/java/com/example/sledipari/utility/Constants.kt
@@ -6,8 +6,8 @@ object Constants {
 
     const val USE_LOCALHOST = true
 
-    const val BASE_URL = "https://desolate-chamber-91023.herokuapp.com/"
-    const val BASE_URL_LOCALHOST = "http://192.168.0.105:1926/" // home
+    const val BASE_URL = BuildConfig.BASE_URL
+    const val BASE_URL_LOCALHOST = BuildConfig.BASE_URL_LOCALHOST
 
     const val ENCRYPTED_SHARED_PREFS_NAME = "ENCRYPTED_SHARED_PREFS_NAME"
     const val KEY_FIREBASE_TOKEN = "KEY_FIREBASE_TOKEN"
