17c710241480fcc00eaae44f8435a677e631467d
Lubomir1998 <lubcho_sk8@abv.bg>
2023-04-14 11:07:03 +0300

    Delete log messages.

--- app/src/main/java/com/example/sledipari/data/MonthRepository.kt
+++ app/src/main/java/com/example/sledipari/data/MonthRepository.kt
@@ -1,8 +1,6 @@
 package com.example.sledipari.data
 
-import android.content.Context
 import android.content.SharedPreferences
-import android.util.Log
 import com.example.sledipari.api.MonthApi
 import com.example.sledipari.api.models.ApiResponse
 import com.example.sledipari.api.models.MonthDTO
@@ -12,11 +10,9 @@ import com.example.sledipari.data.models.Month
 import com.example.sledipari.data.models.CurrencyResponseLocal
 import com.example.sledipari.data.models.Transaction
 import com.example.sledipari.data.models.mapToRates
-import com.example.sledipari.utility.Constants
 import com.example.sledipari.utility.Constants.HISTORY_DURATION
 import com.example.sledipari.utility.Constants.LAST_MODIFIED_DATE
 import com.example.sledipari.utility.extensions.isCurrent
-import com.example.sledipari.utility.formatDate
 import io.ktor.client.call.*
 import io.ktor.http.*
 import javax.inject.Inject
@@ -32,10 +28,7 @@ class MonthRepository @Inject constructor(
         val response = api.checkLastModifiedDate()
         val lastModified = response.headers[HttpHeaders.LastModified]?.toLong() ?: 0L
 
-        Log.d("test dates", "last modified on server ${lastModified.formatDate("dd-MM-yyyy - HH:mm:ss")}, last modified local ${sharedPreferences.getLong(LAST_MODIFIED_DATE, -111L).formatDate("dd-MM-yyyy - HH:mm:ss")}")
-
         if (sharedPreferences.getLong(LAST_MODIFIED_DATE, 0L) < lastModified) {
-            Log.d("test dates", "fetching all months")
             getAllMonths()
         }
     }
@@ -71,7 +64,6 @@ class MonthRepository @Inject constructor(
             val lastModified = response.headers[HttpHeaders.LastModified]?.toLong() ?: 0L
 
             if (success) {
-                Log.d("test dates", "saved locally -> ${lastModified.formatDate("dd-MM-yyyy - HH:mm:ss")}")
                 sharedPreferences.edit().putLong(LAST_MODIFIED_DATE, lastModified).apply()
             }
 
@@ -88,7 +80,6 @@ class MonthRepository @Inject constructor(
             val lastModified = response.headers[HttpHeaders.LastModified]?.toLong() ?: 0L
 
             if (success) {
-                Log.d("test dates", "saved locally -> ${lastModified.formatDate("dd-MM-yyyy - HH:mm:ss")}")
                 sharedPreferences.edit().putLong(LAST_MODIFIED_DATE, lastModified).apply()
             }
 
